919616d6ea66ad65db0ab6b72ec67aa7515284ab
Manuel Ballmer <manuel.ballmer@gmail.com>
2013-08-20 21:35:29 +0200

    Revert "Update category.rb"
    This reverts commit 8d4194d29980ac26eede6240a6ad0f45f67ad0c7.

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,22 +8,10 @@ class Category < ActiveRecord::Base
   end
 
   def self.array
-    x = I18n.locale
-    if x = "de-CH"
-      order('name_de'.to_s + ' ASC').collect { |c| [c.send('name_de'.to_s), c.id] }
-    else
-      order('name_'+ I18n.locale.to_s + ' ASC').collect { |c| [c.send('name_' + I18n.locale.to_s), c.id] }
-
-    end
+    order('name_'+ I18n.locale.to_s + ' ASC').collect { |c| [c.send('name_' + I18n.locale.to_s), c.id] }
   end
 
   def to_s
-    x = I18n.locale
-    if x = "de-CH"
-      self.send('name_de'.to_s)
-    else
-      self.send('name_' + I18n.locale.to_s)
-    end
-  
+    self.send('name_' + I18n.locale.to_s)
   end
 end
