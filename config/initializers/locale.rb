5cfab6ad6ad1e8e36ce0ce8d06982b57d4dab964
Manuel Ballmer <manuel.ballmer@gmail.com>
2013-08-22 11:48:06 +0200

    translation DE

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -6,4 +6,10 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.{yml}')].collect {|l| locale = l.scan(/.+\/(..)\.yml/)[0]; locale[0].to_sym unless locale.nil?}.delete_if {|locale| locale.nil?}
 
 # set default locale to something other than :en
-I18n.default_locale = :de-CH
+I18n.default_locale = :"de-CH"
+I18n.locale = :"de-CH"
+# # using the default locale as default fallback locale
+#
+#I18n.default_locale = :en
+#I18n.fallbacks = I18n::Locale::Fallbacks.new(:"de-CH" => :"de-DE")
+#I18n.fallbacks[:"de-CH"] # => [:"de-CH", :"de-DE", :de, :en]
